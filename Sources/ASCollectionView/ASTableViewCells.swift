58c98f5ae028acd0c5f54cd5e72f49a2ad39ec4f
Apptek Studios <contact@apptekstudios.com>
2020-03-26 13:35:58 +1000

    Make tableViewCells have no backgroundColor by default

--- Sources/ASCollectionView/ASTableViewCells.swift
+++ Sources/ASCollectionView/ASTableViewCells.swift
@@ -31,7 +31,16 @@ class ASTableViewCell: UITableViewCell, ASDataSourceConfigurableCell
 
 	var invalidateLayout: (() -> Void)?
 	var shouldInvalidateLayout: Bool = false
-
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+		backgroundColor = nil
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
 	private(set) var id: ASCollectionViewItemUniqueID?
 
 	func configureAsEmpty()
