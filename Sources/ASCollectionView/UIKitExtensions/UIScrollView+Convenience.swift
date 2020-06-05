6aac5e7bd5741506f6d971b263af8bfa62696e2f
Alexander Kolov <me@alexkolov.com>
2020-06-05 19:45:35 +0900

    Use adjustedContentInset top

--- Sources/ASCollectionView/UIKitExtensions/UIScrollView+Convenience.swift
+++ Sources/ASCollectionView/UIKitExtensions/UIScrollView+Convenience.swift
@@ -8,7 +8,7 @@ extension UIScrollView
 	{
 		CGSize(
 			width: contentSize.width + adjustedContentInset.left + adjustedContentInset.right,
-			height: contentSize.height + adjustedContentInset.bottom + contentInset.top) // NOTE: the adjusted top inset intentionally left out, as SwiftUI uses a negative contentOffset to display the nav bar (doesn't affect content size)
+			height: contentSize.height + adjustedContentInset.bottom + adjustedContentInset.top)
 	}
 
 	var maxContentOffset: CGPoint
