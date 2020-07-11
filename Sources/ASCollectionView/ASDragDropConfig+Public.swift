de016a2e137ae4f831b375c95d5c8995a3f86c14
Toby Brennan <contact@apptekstudios.com>
2020-07-11 11:11:07 +1000

    Document dragDrop

--- Sources/ASCollectionView/ASDragDropConfig+Public.swift
+++ Sources/ASCollectionView/ASDragDropConfig+Public.swift
@@ -21,7 +21,12 @@ public extension ASDragDropConfig
 	/// This allows you to manually implement drag/drop/reordering support
 	/// In the onDelete/onInsert/onMove closures return true if you apply the suggested delete/insert/move, or false if it shouldn't be applied... so that ASCollectionView can correctly animate.
 	/// Use the modifiers to add extra checks (eg .canDragItem, .canMoveItem, .dragItemProvider, .dropItemProvider)
-	init(dragEnabled: Bool = true, dropEnabled: Bool = true, reorderingEnabled: Bool = true, onDeleteOrRemoveItems: ((_ indexSet: IndexSet) -> Bool)? = nil, onInsertItems: ((_ index: Int, _ items: [Data]) -> Bool)? = nil, onMoveItem: ((Int, Int) -> Bool)? = nil)
+	init(dragEnabled: Bool = true,
+		 dropEnabled: Bool = true,
+		 reorderingEnabled: Bool = true,
+		 onDeleteOrRemoveItems: ((_ indexSet: IndexSet) -> Bool)? = nil,
+		 onInsertItems: ((_ index: Int, _ items: [Data]) -> Bool)? = nil,
+		 onMoveItem: ((Int, Int) -> Bool)? = nil)
 	{
 		dataBinding = nil
 		self.onDeleteOrRemoveItems = onDeleteOrRemoveItems
@@ -37,6 +42,7 @@ public extension ASDragDropConfig
 		ASDragDropConfig()
 	}
 
+	/// Called to check whether an item can be dragged
 	func canDragItem(_ closure: @escaping ((IndexPath) -> Bool)) -> Self
 	{
 		var this = self
@@ -44,6 +50,7 @@ public extension ASDragDropConfig
 		return this
 	}
 
+	/// Called to check whether a move should be allowed
 	func canMoveItem(_ closure: @escaping ((IndexPath, IndexPath) -> Bool)) -> Self
 	{
 		var this = self
@@ -51,6 +58,11 @@ public extension ASDragDropConfig
 		return this
 	}
 
+	/// An optional closure that you can use to decide what to do with a dropped item.
+	/// Return nil if you want to ignore the drop.
+	/// Return an item (of the same type as your section data) if you want to insert a row.
+	/// `sourceItem`: If the drop originated from a cell with the same data source, this will provide the original item that has been dragged
+	/// `dragItem`: This is the further information provided by UIKit. For example, if a drag came from another app, you could deal with that using this.
 	func dropItemProvider(_ provider: @escaping ((Data?, UIDragItem) -> Data?)) -> Self
 	{
 		var this = self
@@ -59,6 +71,7 @@ public extension ASDragDropConfig
 		return this
 	}
 
+	/// An optional closure that you can use to provide extra info (eg. for dragging outside of your app)
 	func dragItemProvider(_ provider: @escaping ((_ item: Data) -> NSItemProvider?)) -> Self
 	{
 		var this = self
--- Sources/ASCollectionView/Config/ASDragDropConfig.swift
+++ Sources/ASCollectionView/Config/ASDragDropConfig.swift
@@ -29,14 +29,8 @@ public struct ASDragDropConfig<Data>
 	/// Called to check whether an item can be moved to the specified indexPath
 	var canMoveItem: ((_ sourceIndexPath: IndexPath, _ destinationIndexPath: IndexPath) -> Bool)?
 
-	/// An optional closure that you can use to provide extra info (eg. for dragging outside of your app)
 	var dragItemProvider: ((_ item: Data) -> NSItemProvider?)?
 
-	/// An optional closure that you can use to decide what to do with a dropped item.
-	/// Return nil if you want to ignore the drop.
-	/// Return an item (of the same type as your section data) if you want to insert a row.
-	/// `sourceItem`: If the drop originated from a cell with the same data source, this will provide the original item that has been dragged
-	/// `dragItem`: This is the further information provided by UIKit. For example, if a drag came from another app, you could deal with that using this.
 	var dropItemProvider: ((_ sourceItem: Data?, _ dragItem: UIDragItem) -> Data?)?
 
 	init()
