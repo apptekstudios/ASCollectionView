9a439e3bdbaba61e134b77da51efbf63c72701d7
Toby Brennan <contact@apptekstudios.com>
2020-09-22 11:44:42 +1000

    Fix for iOS14 SwiftUI keyboard safearea

--- Sources/ASCollectionView/Implementation/ASHostingController.swift
+++ Sources/ASCollectionView/Implementation/ASHostingController.swift
@@ -202,9 +202,48 @@ private class AS_UIHostingController<Content: View>: UIHostingController<Content
 		}
 	}
 
-	override func loadView()
+	func disableSafeArea()
 	{
-		super.loadView()
+		guard let viewClass = object_getClass(view) else { return }
+
+		let viewSubclassName = String(cString: class_getName(viewClass)).appending("_IgnoreSafeArea")
+		if let viewSubclass = NSClassFromString(viewSubclassName)
+		{
+			object_setClass(view, viewSubclass)
+		}
+		else
+		{
+			guard let viewClassNameUtf8 = (viewSubclassName as NSString).utf8String else { return }
+			guard let viewSubclass = objc_allocateClassPair(viewClass, viewClassNameUtf8, 0) else { return }
+
+			if let method = class_getInstanceMethod(UIView.self, #selector(getter: UIView.safeAreaInsets))
+			{
+				let safeAreaInsets: @convention(block) (AnyObject) -> UIEdgeInsets = { _ in
+					.zero
+				}
+				class_addMethod(viewSubclass, #selector(getter: UIView.safeAreaInsets), imp_implementationWithBlock(safeAreaInsets), method_getTypeEncoding(method))
+			}
+
+			if let method2 = class_getInstanceMethod(viewClass, NSSelectorFromString("keyboardWillShowWithNotification:"))
+			{
+				let keyboardWillShow: @convention(block) (AnyObject, AnyObject) -> Void = { _, _ in }
+				class_addMethod(viewSubclass, NSSelectorFromString("keyboardWillShowWithNotification:"), imp_implementationWithBlock(keyboardWillShow), method_getTypeEncoding(method2))
+			}
+
+			objc_registerClassPair(viewSubclass)
+			object_setClass(view, viewSubclass)
+		}
+	}
+
+	override init(rootView: Content)
+	{
+		super.init(rootView: rootView)
+		disableSafeArea()
 		disableInteractionsIfNeeded()
+	}
+
+	@objc dynamic required init?(coder aDecoder: NSCoder)
+	{
+		fatalError("init(coder:) has not been implemented")
 	}
 }
